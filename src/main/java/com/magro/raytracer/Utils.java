768aba393c0505ad569c78318a75ff1d106dd1c9
Magro28 <turgeon28@gmail.com>
2020-11-13 11:26:30 +0100

    Remove new lines

--- src/main/java/com/magro/raytracer/Utils.java
+++ src/main/java/com/magro/raytracer/Utils.java
@@ -3,11 +3,9 @@ package com.magro.raytracer;
 public class Utils {
 
     // Constants
-
     public static final double infinity = Double.POSITIVE_INFINITY;
 
     // Utility Functions
-
     public static double degreesToRadians(double degrees) {
         return degrees * Math.PI / 180.0;
     }
